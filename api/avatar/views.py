7314240c951b71dbbab4365a875e5ab61e34fcac
andrebargas <andrebargas@gmail.com>
2018-11-07 14:36:00 -0200

    Avatar with images

--- api/avatar/views.py
+++ api/avatar/views.py
@@ -11,35 +11,3 @@ class AvatarViewSet(viewsets.ModelViewSet):
 
     queryset = Avatar.objects.all()
     serializer_class = AvatarSerializer
-
-@permission_classes((IsAuthenticatedOrReadOnly,))
-class getAvatarImageView(APIView):
-    def getimage(request, format=None):
-        # image_data = open("/avatar_images/download.jpeg", "rb").read()
-        return Response(status=status.HTTP_200_OK)
-
-
-
-
-
-
-
-
-#     def get(self, format=None):
-#         queryset = queryset
-#         serializer = ItenSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request):
-#
-#         serializer = ItenSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=ValueError):
-#             serializer.create(validated_data=request.data)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.error_messages,
-#                         status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class PlayerInvetoryViewSet(viewsets.ModelViewSet):
-#     queryset = PlayerInventory.objects.all()
-#     serializer_class = PlayerInventorySerializer
--- api/gamaron/urls.py
+++ api/gamaron/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from rest_framework import routers
 from users.views import UserViewSet, UserPlayerViewSet, GroupViewSet, PlayerEvolveView
 from itens.views import ItenViewSet, PlayerInvetoryViewSet
-from avatar.views import AvatarViewSet, getAvatarImageView
+from avatar.views import AvatarViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -24,5 +24,4 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^playerevolve/', PlayerEvolveView.as_view(), name='PlayerEvolve'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('static/', getAvatarImageView.as_view(), name='avatar_images')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
