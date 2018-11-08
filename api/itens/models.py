7a0a96cc69986d1fc44ebd8701a906e81260afc1
andrebargas <andrebargas@gmail.com>
2018-11-08 09:31:32 -0200

    Adding avatar field to itens

--- api/itens/models.py
+++ api/itens/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from users.models import UserPlayer
+from avatar.models import Avatar
 from django.db import models
 from django.core.validators import MinLengthValidator
 
@@ -9,7 +10,13 @@ class Iten(models.Model):
                             blank=False, verbose_name="Name")
     description = models.TextField(verbose_name="Description", max_length=250,
                                    blank = False)
+    avatar = models.ForeignKey(Avatar, null=True, blank = False, verbose_name="Description", on_delete=models.CASCADE)
+
+
 
 class PlayerInventory(models.Model):
     player = models.ForeignKey( UserPlayer, on_delete=models.CASCADE, verbose_name="Player" )
-    itens = models.ManyToManyField( Iten, blank = True, verbose_name="Itens" )
+    itens = models.ManyToManyField( Iten, blank=True, verbose_name="Itens" )
+
+    # generate QR code to tickets function
+    # def generateQRCode(self):
